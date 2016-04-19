3767415d574891443c42a3305e93a9cd25d27734
kuchwarh <kuchwarh@aim.com>
2016-04-19 15:31:54 -0400

    class_cats Biology Inserts

--- sql/populate.sql
+++ sql/populate.sql
@@ -7,5 +7,12 @@ insert into fields (name, school, type) values ("csci", "mcas", "major");
 insert into fields (name, school, type) values ("csci", "mcas", "minor");
 
 # Populate class_cats:
-
+insert into class_cats (title) values ("Molecules & Cells");
+insert into class_cats (title) values ("Ecology & Evolution");
+insert into class_cats (title) values ("Investigations in Molecular Cell Biology Lab");
+insert into class_cats (title) values ("Genes and Genomes");
+insert into class_cats (title) values ("Organismal and Systems Biology");
+insert into class_cats (title) values ("Advanced Experience");
+insert into class_cats (title) values ("Additional Biology Electives");
+ 
 # Populate reqs:
