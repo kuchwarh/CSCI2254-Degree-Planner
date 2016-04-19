a11e622316eb08a284ff89a12f6f34311302381b
gonzalyz <gonzalyz@bc.edu>
2016-04-19 15:06:17 -0400

    start populating

---
+++ sql/populate.sql
@@ -0,0 +1,11 @@
+# Populate students:
+
+# Populate fields of study:
+
+insert into fields (name, type) values ("university core", "core");
+insert into fields (name, school, type) values ("csci", "mcas", "major");
+insert into fields (name, school, type) values ("csci", "mcas", "minor");
+
+# Populate class_cats:
+
+# Populate reqs:
