938f8fefaf1081d1903d2daa8cbe10eec1908e7c
gonzalyz <gonzalyz@bc.edu>
2016-04-20 19:28:36 -0400

    add student use case

--- sql/usecases.sql
+++ sql/usecases.sql
@@ -3,11 +3,14 @@
 # View core requirements:
 # actor: student
 
+
 # View major requirements:
 # actor: student
 
+
 # Add field of study:
 # actor: student
+
 
 # Schedule courses:
 # actor: student
@@ -23,11 +26,21 @@
 	# Mark req as AP:
 	insert into aps (student, req) values ($id, $req);
 	
+	
+# Add student:
+# actor: admin
+insert into students (username, password, school) values ($username, sha1($password), $school);
+insert into plan (slot, student) values (1, $id);
+	-- perform insert into plan for slots 1 - 48.
+	
+	
 # Add core requirements:
 # actor: admin
 
+
 # Add major requirements:
 # actor: admin
+
 
 # View student schedule:
 # actor: admin
