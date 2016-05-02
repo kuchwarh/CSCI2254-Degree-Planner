a2d6db2fa4acd2e08c646bdf594dc5f96b88fad5
gonzalyz <gonzalyz@bc.edu>
2016-05-02 10:33:38 -0400

    student sign up

---
+++ php/signupstudent.php
@@ -0,0 +1,51 @@
+<?php include("dbconn.php");
+
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$school = $_POST['school'];
+	
+	function checkname($name) {
+		$dbc = connect_to_db('gonzalyz');
+		$result = perform_query($dbc, "select * from students where username = '$name'");
+		$matches = mysqli_num_rows($result);
+		disconnect_from_db($dbc, $result);
+		return ($matches == 1);
+	};
+	
+	if (checkname($username) > 0) {
+		echo "<script>alert('Username in use.');
+		window.location.href='../signup.html';
+		</script>";
+	} else {
+	
+	$dbc = connect_to_db('gonzalyz');
+	$insertstud = "insert into students (username, password, school) values ('$username', sha1('$password'), '$school')";
+	perform_query($dbc, $insertstud);
+	
+	$getslots = "select id from slots";
+	$result = perform_query($dbc, $getslots);
+	$slots = array();
+	while ($obj = mysqli_fetch_object($result)) {
+		$slots[] = $obj;
+	};
+	
+	function getID($name) {
+		$dbc = connect_to_db('gonzalyz');
+		$result = perform_query($dbc, "select id from students where username = '$name'");
+		$row = mysqli_fetch_array($result);
+		return $row['id'];
+	};
+	$studid = getID($username);
+	
+	foreach ($slots as $slot) {
+		$insertslot = "insert into plan (slot, student) values ('$slot->id', '$studid')";
+		perform_query($dbc, $insertslot);
+	};
+	
+	setcookie('loginUserID', getID($_POST['username']), time() + 60*60*24*30, '/');
+	
+	echo "<script>alert('Student successfully signed-up.');
+		window.location.href='../studenthome.html';
+		</script>";
+	
+	};
