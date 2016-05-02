21ce303620b9f86842e39f92ce12f744466b57e5
kuchwarh <kuchwarh@aim.com>
2016-05-02 19:58:06 -0400

    Correct APCORE appending to table

--- js/core.js
+++ js/core.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
          
-    
+    //just the core part
             	$.ajax({
 		          url: "php/coretable.php",
 		          success: function(data) {
@@ -9,6 +9,41 @@ $(document).ready(function(){
                      var string ="";
                                
                                string = string+"<tr><td>" + item.title +"</td><td><input class ='boxes' type='checkbox' name='count' value='"+item.title+"'/>&nbsp;";
+                               
+                               if(item.number>1){
+                                   string = string +"<input class ='boxes' type='checkbox' name='count' value='"+item.title+"'/>&nbsp;";
+                               }
+                               
+                               string = string + "</td><td><select name = 'Semester'><br><br><option value='Spring' id ='Spring'>"+item.number+"</option> <option value='Fall' id='Fall'>Fall</option></select>";
+                               
+                               
+                                if(item.number>1){
+                                   string = string +"<select name = 'Semester'><br><br><option value='Spring' id ='Spring'>"+item.semester+"</option> <option value='Fall' id='Fall'>Fall</option></select>";
+                               }
+                
+                               string = string + "</td></tr>";
+                               $("#viewcore").append(string);          
+
+			
+				 });
+				
+			},
+            
+		async: true,
+        dataType: "json",
+        error: function(req, err) {console.log(err);}
+	
+		}); 
+    
+    //checked ap part of table 
+            	$.ajax({
+		          url: "php/apcore.php",
+		          success: function(data) {
+                      console.log(JSON.stringify(data));
+			     $.each(data, function(i, item) {
+                     var string ="";
+                               
+                               string = string+"<tr><td>" + item.title +"</td><td><input class ='boxes' type='checkbox' checked='checked' name='count' value='"+item.title+"'/>&nbsp;";
                                
                                if(item.number>1){
                                    string = string +"<input class ='boxes' type='checkbox' name='count' value='"+item.title+"'/>&nbsp;";
---
+++ php/apcore.php
@@ -0,0 +1,24 @@
+<?php 
+
+include('dbconn.php');
+
+$id = $_COOKIE['loginUserID'];
+
+$dbc = connect_to_db('gonzalyz');
+
+$query = "select c.title
+					  from class_cats as c,reqs as r
+					  inner join aps as a
+					  on r.id = a.req
+	 				  where a.student = '$id'
+	 				  and r.class_cat = c.id";
+$result = perform_query($dbc, $query);
+    
+$core_table = array();	// put the rows as objects in an array
+
+while ( $obj = mysqli_fetch_object( $result ) ) {
+		$core_table[] = $obj;
+	}
+
+
+echo json_encode($core_table);
