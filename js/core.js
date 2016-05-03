980aa9e5e212dcbe0c4d92ad7439026e1d801745
kuchwarh <kuchwarh@aim.com>
2016-05-02 22:33:24 -0400

    Correctly Populating Drop Downs for Reqs with SWITCH Statements

--- js/core.js
+++ js/core.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
                                    string = string +"<input class ='boxes' type='checkbox' name='count' value='"+item.title+"'/>&nbsp;";
                                }
                                
-                               string = string + "</td><td><select name = 'Semester'><br><br><option value='Spring' id ='Spring'>"+item.number+"</option> <option value='Fall' id='Fall'>Fall</option></select>";
+                               string = string + "</td><td><select name = 'Semester'><option value=''>&nbsp;</option><option value='Fall1'>First Year, Fall</option><option value='Spring1'>First Year, Spring</option><option value='Fall2'>Second Year, Fall</option><option value='Spring2'>Second Year, Spring</option><option value='Fall3'>Third Year, Fall</option><option value='Spring3'>Third Year, Spring</option><option value='Fall4'>Fourth Year, Fall</option><option value='Spring4'>Fourth Year, Spring</option></select>";
                                
                                
                                 if(item.number>1){
@@ -46,11 +46,10 @@ $(document).ready(function(){
                                string = string+"<tr><td>" + item.title +"</td><td><input class ='boxes' type='checkbox' checked='checked' name='count' value='"+item.title+"'/>&nbsp;";
                                
                                if(item.number>1){
-                                   string = string +"<input class ='boxes' type='checkbox' checked='checked' name='count' value='"+item.title+"'/>&nbsp;";
+                                   string = string +"<input class ='boxes' type='checkbox' name='count' value='"+item.title+"'/>&nbsp;";
                                }
                                
-                               string = string + "</td><td><select name = 'Semester'><br><br><option value='Spring' id ='Spring'>"+item.number+"</option> <option value='Fall' id='Fall'>Fall</option></select>";
-                               
+                               string = string + "</td><td><select name = 'Semester'><option value=''>&nbsp;</option><option value='Fall1'>First Year, Fall</option><option value='Spring1'>First Year, Spring</option><option value='Fall2'>Second Year, Fall</option><option value='Spring2'>Second Year, Spring</option><option value='Fall3'>Third Year, Fall</option><option value='Spring3'>Third Year, Spring</option><option value='Fall4'>Fourth Year, Fall</option><option value='Spring4'>Fourth Year, Spring</option></select>";
                                
                                 if(item.number>1){
                                    string = string +"<select name = 'Semester'><br><br><option value='Spring' id ='Spring'>"+item.semester+"</option> <option value='Fall' id='Fall'>Fall</option></select>";
@@ -68,10 +67,9 @@ $(document).ready(function(){
         dataType: "json",
         error: function(req, err) {console.log(err);}
 	
-		}); 
+		});
     
-    
-    //checked reqs part of table 
+        //checked reqs part of table 
             	$.ajax({
 		          url: "php/reqscore.php",
 		          success: function(data) {
@@ -84,13 +82,45 @@ $(document).ready(function(){
                                if(item.number>1){
                                    string = string +"<input class ='boxes' type='checkbox' name='count' value='"+item.title+"'/>&nbsp;";
                                }
-                               
-                               string = string + "</td><td><select name = 'Semester'><br><br><option value='Spring' id ='Spring'>"+item.number+"</option> <option value='Fall' id='Fall'>Fall</option></select>";
+                 
+                     
+                            switch (item.semester) {
+                                    case "fall":        
+                            switch (item.year) {
+                                    case "1":
+                                     string = string + "</td><td><select name = 'Semester'><option value=''>&nbsp;</option><option value='Fall1' selected>First Year, Fall</option><option value='Spring1'>First Year, Spring</option><option value='Fall2'>Second Year, Fall</option><option value='Spring2'>Second Year, Spring</option><option value='Fall3'>Third Year, Fall</option><option value='Spring3'>Third Year, Spring</option><option value='Fall4'>Fourth Year, Fall</option><option value='Spring4'>Fourth Year, Spring</option></select>";
+                                        break;
+                                    case "2":
+                                    string = string + "</td><td><select name = 'Semester'><option value=''>&nbsp;</option><option value='Fall1'>First Year, Fall</option><option value='Spring1'>First Year, Spring</option><option value='Fall2' selected>Second Year, Fall</option><option value='Spring2'>Second Year, Spring</option><option value='Fall3'>Third Year, Fall</option><option value='Spring3'>Third Year, Spring</option><option value='Fall4'>Fourth Year, Fall</option><option value='Spring4'>Fourth Year, Spring</option></select>";
+                                        break;
+                                    case "3":   
+                                        break;
+                                    case "4":
+                                        break;
+                                }
+                                    break;
+                                  case "spring":
+                                    
+                            switch (item.year) {
+                                    case "1":
+                                        break;
+                                    case "2":
+                                        break;
+                                    case "3":
+                                        break;
+                                    case "4":
+                                        break;
+                                }
+                                    break;
+                                              }
+                                    
                                
                                
                                 if(item.number>1){
                                    string = string +"<select name = 'Semester'><br><br><option value='Spring' id ='Spring'>"+item.semester+"</option> <option value='Fall' id='Fall'>Fall</option></select>";
                                }
+                               
+                             
                 
                                string = string + "</td></tr>";
                                $("#viewcore").append(string);          
