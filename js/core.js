a3015ef0fb229500a4640f019ab23350c159fbb0
kuchwarh <kuchwarh@aim.com>
2016-05-02 20:07:36 -0400

    Don't get for aps

--- js/core.js
+++ js/core.js
@@ -46,6 +46,42 @@ $(document).ready(function(){
                                string = string+"<tr><td>" + item.title +"</td><td><input class ='boxes' type='checkbox' checked='checked' name='count' value='"+item.title+"'/>&nbsp;";
                                
                                if(item.number>1){
+                                   string = string +"<input class ='boxes' type='checkbox' checked='checked' name='count' value='"+item.title+"'/>&nbsp;";
+                               }
+                               
+                               string = string + "</td><td><select name = 'Semester'><br><br><option value='Spring' id ='Spring'>"+item.number+"</option> <option value='Fall' id='Fall'>Fall</option></select>";
+                               
+                               
+                                if(item.number>1){
+                                   string = string +"<select name = 'Semester'><br><br><option value='Spring' id ='Spring'>"+item.semester+"</option> <option value='Fall' id='Fall'>Fall</option></select>";
+                               }
+                
+                               string = string + "</td></tr>";
+                               $("#viewcore").append(string);          
+
+			
+				 });
+				
+			},
+            
+		async: true,
+        dataType: "json",
+        error: function(req, err) {console.log(err);}
+	
+		}); 
+    
+    
+    //checked reqs part of table 
+            	$.ajax({
+		          url: "php/reqscore.php",
+		          success: function(data) {
+                      console.log(JSON.stringify(data));
+			     $.each(data, function(i, item) {
+                     var string ="";
+                               
+                               string = string+"<tr><td>" + item.title +"</td><td><input class ='boxes' type='checkbox' name='count' value='"+item.title+"'/>&nbsp;";
+                               
+                               if(item.number>1){
                                    string = string +"<input class ='boxes' type='checkbox' name='count' value='"+item.title+"'/>&nbsp;";
                                }
                                
