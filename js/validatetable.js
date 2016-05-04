bfe434f68d42d3768ddeefbdce9338dd8ea5d6e0
gonzalyz <gonzalyz@bc.edu>
2016-05-03 20:15:50 -0400

    fixed table validation

--- js/validatetable.js
+++ js/validatetable.js
@@ -3,22 +3,29 @@ $(document).ready(function() {
 	function checkTable(event) {
 
 		var table = document.getElementById("viewcore");
-		var inputs = table.getElementsByTagName("input");
+		var checks = document.getElementsByName("count[]");
+		console.log(checks);
+		var selects = table.getElementsByTagName("select");
+		console.log(selects);
 	
-		for (var i = 0; i < inputs.length; i += 2) {
-			var check = inputs[i];
-			var select = inputs [i + 1];
-		
-			if (!(check.value == "") || !(check.value == null)) {
-				if (!(select.value == "") || !(select.value == null)) {
-					alert("Requirements cannot be marked as AP credit and planned for a semester.");
-					event.preventDefault();
-					return false;
-				} else {
-					return true;
-				};
+		var overlap = false;
+		for (var i = 0; i < checks.length; i++) {
+			var check = checks[i];
+			console.log(check.checked);
+			var select = selects[i];
+			console.log(select.value);
+			
+			if (check.checked && !(select.value == "")) {
+				overlap = true;
 			};
-		
+		};
+			
+		if (overlap) {				
+				alert("Requirements cannot be marked as AP credit and planned for a semester.");
+				event.preventDefault();
+				return false;
+		} else {
+			return true;
 		};
 
 	};
